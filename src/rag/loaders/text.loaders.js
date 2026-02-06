1c0f78e42e1fcde7ae355be45a3d445eae2d985d
Kishore868 <kishorechandra.cs23@bmsce.ac.in>
2026-02-06 18:29:00 +0530

    Revert "dummy commit"
    This reverts commit 94aa5d782fbc6cbce155ec7eb381b264b0e1f806.

--- src/rag/loaders/text.loaders.js
+++
@@ -1,5 +0,0 @@
-import fs from "fs/promises";
-
-export const loadTextFile = async (filePath) => {
-  return fs.readFile(filePath, "utf-8");
-};
--- src/rag/retrieval/retrieve.service.js
+++
@@ -1,7 +0,0 @@
-import { getVectorStore } from "../ingestion/ingest.service.js";
-
-export const retrieveRelevantChunks = async ({ question, k = 4 }) => {
-  const store = getVectorStore();
-  const results = await store.similaritySearch(question, k);
-  return results.map(d => d.pageContent).join("\n\n");
-};
