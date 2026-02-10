dfa2f1b134d83cdb45ad67af2f7579e4504f5914
Kishore868 <kishorechandra.cs23@bmsce.ac.in>
2026-02-10 13:48:16 +0530

    minor error

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+
 app.use(express.urlencoded({ extended: true }));
  // Adjust the origin as needed
 app.use(express.json({limit: "100mb"})); 
